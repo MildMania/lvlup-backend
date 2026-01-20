aa6a822a78226b98ba66345bba19315d428ff067
Emre <e@mildmania.com>
2026-01-20 14:35:58 +0300

    analytics controller issue fixed

--- backend/src/controllers/AnalyticsController.ts
+++ backend/src/controllers/AnalyticsController.ts
@@ -179,7 +179,7 @@ export class AnalyticsController {
 
                 // Prevent future dates
                 if (startDate > new Date()) {
-                    logger.warning(
+                    logger.warn(
                         'startTime is in the future, resetting to current time. Client Timestamp:' 
                         , sessionData.startTime, " Server Timestamp:", new Date().toISOString());
                     sessionData.startTime = new Date().toISOString();

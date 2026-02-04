24eff0946e48a4d4dae67642a9532988d5047997
Emre <e@mildmania.com>
2026-02-04 18:48:15 +0300

    removed all migrations

--- backend/prisma/migrations/20260204120000_add_dashboard_indexes/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "events_gameId_timestamp_idx" ON "events"("gameId", "timestamp");
-
--- CreateIndex
-CREATE INDEX "events_gameId_userId_timestamp_idx" ON "events"("gameId", "userId", "timestamp");
